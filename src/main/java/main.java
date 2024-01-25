163fc4853670c93d1b16270cfd7fb53f308c8b1a
Alaa EL Homsany <42380324+aelhomsany@users.noreply.github.com>
2024-01-25 17:18:11 +0200

    add rmq connection and methods

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,3 +1,5 @@
+import utils.MqUtils;
+
 import java.awt.event.InputEvent;
 import java.util.logging.Level;
 
@@ -5,6 +7,10 @@ class Simple{
     public static void main(String[] args){
 
             System.out.println("Hello Java");
+        MqUtils mqUtils = new MqUtils();
+        mqUtils.connectToRabbitMQ();
+        mqUtils.purgeQueue("q_bundles_categories_import_preprod");
+        mqUtils.closeConnection();
 
     }
 }  
